476b49ec64a93f470f987369b891372df6ad4863
Wania Sharif <w0842810@myscc.ca>
2025-01-24 21:32:56 -0500

    #31 Create section for details and type selection

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -3,7 +3,16 @@ import '../styles/productDetails.css'
 function ProductDetails() {
     return (
       <div className="detailsPage">
-        
+        <div className="details">
+            <h4>Product Name</h4>
+            <h6>$29.99</h6>
+            <p>sjfdj skd fsk dfskkjah</p>
+            <form>
+                <h5>Colour</h5>
+                <h5>Size</h5>
+                <button type="submit">Add to cart</button>
+            </form>
+        </div>
       </div>
     );
   }
