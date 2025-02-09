a0b9ab34f6d6aa43b3f11399a2a8cc6cf09fe14d
George Secen <W0533223@myscc.ca>
2025-02-08 23:39:26 -0500

    Add controller function createProduct
    - Added productController function createProduct which creates a product in the database

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,7 +2,7 @@ const Product = require('../models/productModel');
 const Image = require('../models/productImageModel');
 
 //Fetch all products
-const getAllProducts = async (req, res) => {
+exports.getAllProducts = async (req, res) => {
     try {
         const products = await Product.findAll({
             //Join tables
@@ -23,7 +23,7 @@ const getAllProducts = async (req, res) => {
 }
 
 //Add product to the cart
-const addProduct = async (req, res) => {
+exports.addProduct = async (req, res) => {
     try {
         //TODO delete extra object fields later
         const { id, name, description, price, image, stock_quantity } = req.body;
@@ -37,4 +37,32 @@ const addProduct = async (req, res) => {
     }
 }
 
-module.exports = { getAllProducts, addProduct }
+/**
+ * Creates a product in the database.
+ * @param {Object} request Express js request object.
+ * @param {Object} response Express js response object.
+ */
+exports.createProduct = async (request, response) => {
+
+    const {name, description, price} = request.body
+
+    try{
+
+        // Create product
+        await Product.create({
+            name: name,
+            description: description,
+            price: price,
+        })
+
+        console.log("Product created successfully in database.")
+        response.status(200).json({
+            message: "Product created successfully in database.",
+        })
+    } 
+    catch (error){
+        console.log(`Error in productController.js function createProduct: ${error.message}`)
+        response.status(500).json({error: error.message})
+    }
+}
+
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getAllProducts, addProduct } = require('../controllers/productController');
+const { getAllProducts, addProduct, createProduct } = require('../controllers/productController');
 const router = express.Router();
 
 router.get('/', getAllProducts);
 router.post('/', addProduct);  // This would require authentication and authorization in a real app
+
+router.post("/create-product", createProduct)
 
 module.exports = router;
