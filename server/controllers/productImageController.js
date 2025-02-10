f0dcc2bb03376d14270f3c33f45ea936d2614b6d
George Secen <W0533223@myscc.ca>
2025-02-10 00:40:53 -0500

    Add image to server
    - Added functionality to the productImageController function createImage to locate the images path
    on the server and add an image to it

--- server/controllers/productImageController.js
+++ server/controllers/productImageController.js
@@ -1,4 +1,6 @@
 const Image = require('../models/productImageModel');
+const fs = require('fs')
+const path = require('path')
 
 /**
  * Creates a image in the database.
@@ -8,20 +10,34 @@ const Image = require('../models/productImageModel');
 exports.createImage = async (request, response) => {
 
     const {productId, productVariantId, isPrimary, url} = request.body
+    const {originalname, encoding, buffer} = request.file
 
     try{
 
         // Create image
-        await Image.create({
+        const image = await Image.create({
             product_id: productId,
             product_variant_id: productVariantId,
             is_primary: isPrimary,
             image_url: url
         })
 
-        console.log("Image created successfully in database.")
+        // Note: When working with paths avoid using slashes at all costs as different operating 
+        // systems use different slashes
+
+        // Get path to images folder on server
+        const serverPath = path.dirname(__dirname)
+        const imagesPath = path.join(serverPath, "public", "images")        
+        
+        // Add image to server
+        // Note: Images with same name will be overwritten
+        await fs.promises.writeFile(path.join(imagesPath, `test${path.extname(originalname)}`), buffer)
+
+        console.log(request.file)
+
+        console.log("Image created successfully in database and added to server.")
         response.status(200).json({
-            message: "Image created successfully in database.",
+            message: "Image created successfully in database and added to server.",
         })
     } 
     catch (error){
