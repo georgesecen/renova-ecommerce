77bb14aef78f05d23ad71656849196976d6ae760
George Secen <W0533223@myscc.ca>
2025-02-12 21:06:13 -0500

    Add controller function updateProductVariant
    - Added productVariantController function updateProductVariant which updates a product variant in the database
    
    - Added route to update product variant

--- server/controllers/productVariantController.js
+++ server/controllers/productVariantController.js
@@ -1,4 +1,4 @@
-const ProductVariantModel = require("../models/productVariantModel")
+const ProductVariant = require("../models/productVariantModel")
 
 /**
  * Creates a product variant in the database.
@@ -12,7 +12,7 @@ exports.createProductVariant = async (request, response) => {
     try{
 
         // Create product variant
-        await ProductVariantModel.create({
+        await ProductVariant.create({
             product_id: productId,
             color: color,
             size: size,
@@ -27,6 +27,44 @@ exports.createProductVariant = async (request, response) => {
     } 
     catch (error){
         console.log(`Error in productVariantController.js function createProductVariant: ${error.message}`)
+        response.status(500).json({error: error.message})
+    }
+}
+
+/**
+ * Updates a product variant in the database.
+ * @param {Object} request Express js request object.
+ * @param {Object} response Express js response object.
+ */
+exports.updateProductVariant = async (request, response) => {
+
+    const {id, color, size, quantity, price} = request.body
+
+    try{
+
+        // Get product variant
+        const productVariant = await ProductVariant.findByPk(id)
+
+        // If product variant  does not exist
+        if (productVariant == null){
+            throw new Error(`Product variant with ID ${id} does not exist in database.`)
+        }
+
+        // Update product variant
+        await productVariant.update({
+            color: color,
+            size: size,
+            stock_quantity: quantity,
+            price: price
+        })
+
+        console.log("Product variant updated successfully in database.")
+        response.status(200).json({
+            message: "Product variant updated successfully in database.",
+        })
+    } 
+    catch (error){
+        console.log(`Error in productVariantController.js function updateProductVariant: ${error.message}`)
         response.status(500).json({error: error.message})
     }
 }
--- server/routes/productVariant.js
+++ server/routes/productVariant.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createProductVariant } = require('../controllers/productVariantController');
+const { createProductVariant, updateProductVariant } = require('../controllers/productVariantController');
 const router = express.Router();
 
 // TODO: Make routes admin only routes
 router.post("/create-product-variant", createProductVariant)
+router.post("/update-product-variant", updateProductVariant)
 
 module.exports = router;
