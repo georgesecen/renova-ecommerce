9571b8d696704e90abe2c3be2bc85de0f5a26d50
George Secen <W0533223@myscc.ca>
2025-02-12 21:52:25 -0500

    Add controller function deleteImage
    - Added productImageController function deleteImage which deletes an image in the database and on the server
    
    - Added route to delete image
    
    - Added the public/images folder to the gitignore so anytime a new image is added or removed git will not
    track it as its unnecessary

--- server/controllers/productImageController.js
+++ server/controllers/productImageController.js
@@ -51,3 +51,46 @@ exports.createImage = async (request, response) => {
         response.status(500).json({error: error.message})
     }
 }
+
+/**
+ * Deletes an image in the database and on the server.
+ * @param {Object} request Express js request object.
+ * @param {Object} response Express js response object.
+ */
+exports.deleteImage = async (request, response) => {
+
+    const {id} = request.body
+
+    try{
+
+        // Get image from database
+        const image = await Image.findByPk(id)
+
+        // If image does not exist
+        if (image == null){
+            throw new Error(`Image with ID ${id} does not exist in database.`)
+        }
+
+        // Get image name so we know what image to delete on server
+        const imageName = image.image_url
+
+        // Delete image on database
+        await image.destroy()
+
+        // Get path to image on server
+        const serverPath = path.dirname(__dirname)
+        const imagePath = path.join(serverPath, "public", "images", imageName)   
+
+        // Delete image on server
+        await fs.promises.rm(imagePath)
+
+        console.log("Image deleted successfully in database and on server.")
+        response.status(200).json({
+            message: "Image deleted successfully in database and on server.",
+        })
+    } 
+    catch (error){
+        console.log(`Error in productImageController.js function deleteImage: ${error.message}`)
+        response.status(500).json({error: error.message})
+    }
+}
--- server/routes/productImage.js
+++ server/routes/productImage.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createImage } = require('../controllers/productImageController');
+const { createImage, deleteImage } = require('../controllers/productImageController');
 const router = express.Router();
 
 // Multer is middleware used for handling multipart/form-data
@@ -9,6 +9,8 @@ const multer = require('multer');
 // Storage is set so image sent to server is stored in memory
 const imageMiddleware = multer({storage: multer.memoryStorage()});
 
+// TODO: Make routes admin only
 router.post('/create-image', imageMiddleware.single("image"), createImage);
+router.post('/delete-image', deleteImage);
 
 module.exports = router;
