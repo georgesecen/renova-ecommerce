6afc3cf72091c5f0f202a7a6f54ef2590f82e3b1
Wania Sharif <w0842810@myscc.ca>
2025-02-20 15:46:37 -0500

    Style side nav - Make side nav a flex to center items - Style list items and animate on hover

--- client/src/components/ProductsPage/ProductsPage.js
+++ client/src/components/ProductsPage/ProductsPage.js
@@ -4,6 +4,16 @@ function ProductsPage() {
     return (
         <div className="products-page">
             <div className="side-nav">
+                <ul>
+                <li>ALL</li>
+                <li>HOODIES</li>
+                <li>T-SHIRTS</li>
+                <li>PANTS</li>
+                </ul>
+            </div>
+
+            <div className="products-list">
+
             </div>
         </div>
     );
