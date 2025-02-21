6f5785efc030cbbcca3f5cd88ca43c7359ec3290
Wania Sharif <w0842810@myscc.ca>
2025-02-21 16:40:05 -0500

    #87 Add product image

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -1,8 +1,12 @@
 import './productCard.css';
 
 function ProductCard() {
+    const img = require('../../assets/images/hoodie2.png')
     return (
         <div className='product-card'>
+            <div className="image">
+                <img src={img} alt=""/>
+            </div>
             <div className="name">NAME</div>
             <div className="price">10.99</div>
         </div>
