d2784ebbe696d55a5da011998d6d79745874e25b
Wania Sharif <w0842810@myscc.ca>
2025-02-21 16:04:03 -0500

    #87 add divs for name and price

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -3,7 +3,8 @@ import './productCard.css';
 function ProductCard() {
     return (
         <div className='product-card'>
-
+            <div className="name">NAME</div>
+            <div className="price">10.99</div>
         </div>
     )
 }
