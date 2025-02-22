cb94c7915c8af2b940a9ff1585e4c7f90db167f3
Wania Sharif <w0842810@myscc.ca>
2025-02-21 23:55:19 -0500

    #86 get products from server and map the result onto product cards

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -1,14 +1,14 @@
 import './productCard.css';
 
-function ProductCard() {
+function ProductCard(props) {
     const img = require('../../assets/images/hoodie2.png')
     return (
         <div className='product-card'>
             <div className="image">
                 <img src={img} alt=""/>
             </div>
-            <div className="name">NAME</div>
-            <div className="price">10.99</div>
+            <div className="name">{props.name}</div>
+            <div className="price">{props.price}</div>
         </div>
     )
 }
--- client/src/components/ProductsPage/ProductsPage.js
+++ client/src/components/ProductsPage/ProductsPage.js
@@ -1,9 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './productsPage.css'
 import ProductCard from '../ProductCard/ProductCard';
+import { getProducts } from '../../services/api';
 
 function ProductsPage() {
-    const [filter, filterBy] = useState(0)
+    const [filter, filterBy] = useState(0);
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+            console.log(products)
+            getProducts()
+                .then((response) => {
+                    console.log(response.data[0].image[0]);
+                    setProducts(response.data);
+                    setTimeout(() => setLoading(false), 100);  // Show spinner for 200ms
+                })
+                .catch((error) => {
+                    console.error('Error fetching products:', error);
+                    setLoading(false);
+                });
+        }, []);
+
     return (
         <div className="products-page">
             <div className="side-nav">
@@ -16,17 +34,9 @@ function ProductsPage() {
             </div>
 
             <div className="products-list">
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
+            {products.map((product) => (
+                <ProductCard key={product.id} name={product.name} price={product.price}/>
+            ))}
             </div>
         </div>
     );
