3c5b22cd3ccfef51d1c38ba59e911fc99ea9e5f0
Wania Sharif <w0842810@myscc.ca>
2025-02-22 00:16:46 -0500

    #87 Add add-to-cart button - Set it to display on hover - Changed format of product card to work with button

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -4,12 +4,16 @@ function ProductCard(props) {
     const img = require('../../assets/images/hoodie2.png')
     return (
         <div className='product-card'>
-            <div className="image">
-                <img src={img} alt=""/>
+            <div className='card'>
+                <div className="image">
+                    <img src={img} alt=""/>
+                </div>
+                <div className="name">{props.name}</div>
+                <div className="price">{props.price}</div>
             </div>
-            <div className="name">{props.name}</div>
-            <div className="price">{props.price}</div>
+            <button>ADD TO CART</button>
         </div>
+
     )
 }
 
