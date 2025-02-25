ac4f936f13fb29a3b5f1c42fca59d3ee8924a3ae
George Secen <W0533223@myscc.ca>
2025-02-25 18:40:44 -0500

    Add basic OrdersTable
    - Added basic empty OrdersTable component and OrderCard component

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import SignUp from "./pages/SignUp";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BottomNavBar from "./components/BottomNavBar";
 import { PageNotFound } from "./pages/PageNotFound";
+import OrdersTable from "./components/OrdersTable/OrdersTable";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path='/test-stripe' element={<TestStripe />} />
+        <Route path='/orders' element={<OrdersTable />} />
         <Route path='/return' element={<Return />} />
       </Routes>
       <BottomNavBar />
---
+++ client/src/components/OrdersTable/OrdersTable.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import OrderCard from './OrderCard/OrderCard'
+
+const OrdersTable = () => {
+  return (
+    <div>
+        <OrderCard></OrderCard>
+    </div>
+  )
+}
+
+export default OrdersTable
