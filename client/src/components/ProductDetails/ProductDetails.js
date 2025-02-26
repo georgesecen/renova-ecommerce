9f776d53258b18d2ed2be89bd5f31a32a9c883e7
Wania Sharif <w0842810@myscc.ca>
2025-02-25 19:21:16 -0500

    #74 style addtocart button

--- client/src/components/ProductDetails/ProductDetails.js
+++ client/src/components/ProductDetails/ProductDetails.js
@@ -30,7 +30,7 @@ function ProductDetails() {
             <form>
                 {/* <h5>Colour</h5>
                 <h5>Size</h5> */}
-                <button type="submit">Add to cart</button>
+                <button className="addToCart" type="submit">ADD TO CART</button>
             </form>
         </div>
       </div>
