4b03081a05639a5d64c2e7ea00995c967f65c4e1
George Secen <W0533223@myscc.ca>
2025-02-25 20:14:59 -0500

    Add CardItem to OrderCard
    - Created CardItem component for the OrderCard which will be every label with value below it
    
    - Commented out global text color styles as it was making it hard to see fonts

--- client/src/components/OrdersTable/OrderCard/OrderCard.js
+++ client/src/components/OrdersTable/OrderCard/OrderCard.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./orderCard.css"
+
+// Card item is every label with value below it
+const CardItem = ({label, value}) => {
+  return (
+    <div className='order-card-item-container'> 
+      <h6>{label}</h6>
+      <p>{value}</p>
+    </div>
+  )
+}
 
 const OrderCard = () => {
   return (
-    <div>OrderCard</div>
+    <div className='order-card-container'>
+      OrderCard
+      <CardItem label={"Order ID"} value={"#18"}></CardItem>
+    </div>
   )
 }
 
--- client/src/components/OrdersTable/OrdersTable.js
+++ client/src/components/OrdersTable/OrdersTable.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import OrderCard from './OrderCard/OrderCard'
+import "./ordersTable.css"
 
 const OrdersTable = () => {
   return (
-    <div>
+    <div className='order-table-container'>
         <OrderCard></OrderCard>
     </div>
   )
