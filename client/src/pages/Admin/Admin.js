13b6fadaa38e918d8a38c4840044123cbf087c21
George Secen <W0533223@myscc.ca>
2025-02-26 23:02:35 -0500

    Get admin key
    - Added functionality to get the key which will be the admin password used to access
    sensitive routes. Also stored it in session storage to be more secure as that way
    it is removed from device after session.

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import {React, useEffect} from 'react'
 import "./admin.css"
 
 const Admin = () => {
+
+  // Get key for admin routes
+  useEffect(() => {
+
+    // Store key in session storage for better security
+    sessionStorage.removeItem("key")
+    sessionStorage.setItem("key", prompt("Key:"))
+  })
+
   return (
     <div>Admin</div>
   )
