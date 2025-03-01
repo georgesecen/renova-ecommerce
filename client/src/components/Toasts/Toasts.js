01c60d823dc2cc8a45b716303508a57c8f304fe5
George Secen <W0533223@myscc.ca>
2025-03-01 18:29:36 -0500

    Automatically remove toast
    - Added functionality to automatically remove the toast once user exits the toast or the delay runs
    out

--- client/src/components/Toasts/Toasts.js
+++ client/src/components/Toasts/Toasts.js
@@ -5,10 +5,20 @@ import ToastContainer from 'react-bootstrap/ToastContainer';
 // https://react-bootstrap.netlify.app/docs/components/toasts/
 const Toasts = () => {
   
-  const [toasts, setToasts] = useState([]) // [{title: toast title, message: toast messsage, show: true/false}]
+  const [toasts, setToasts] = useState([{title:"test title", message:"test message", show:true}]) // [{title: toast title, message: toast messsage, show: true/false}]
 
+  // Function adds a toast to the toasts list and will be used by other components
+  // so the other components can easily send notifications.
   function addToast(title, message){
-    setToasts([...toasts, {title: title, message: message}])
+    setToasts([...toasts, {title: title, message: message, show: true}])
+  }
+
+  // Function will automatically be called once user exits the toast or the 
+  // delay runs out. Afterwards toast will be set to show = false and will
+  // never be displayed again.
+  function removeToast(index){
+    toasts[index].show = false
+    setToasts([...toasts])
   }
 
   console.log(toasts)
@@ -17,9 +27,9 @@ const Toasts = () => {
     <ToastContainer position='top-center'>
 
         {
-            toasts.map(({title, message}, index) =>{
+            toasts.map(({title, message, show}, index) =>{
                 return(
-                    <Toast key={index}>
+                    <Toast key={index} onClose={()=>{removeToast(index)}} show={show} delay={3000} autohide>
                         <Toast.Header>
                             <strong className="me-auto">{title}</strong>
                             <small className="text-muted">just now</small>
