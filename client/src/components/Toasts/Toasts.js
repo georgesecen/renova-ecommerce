fb3cc7212428200edc9b7b218e2dbfa252084807
George Secen <W0533223@myscc.ca>
2025-03-01 18:18:22 -0500

    Dynamically display toast in Toasts component

--- client/src/components/Toasts/Toasts.js
+++ client/src/components/Toasts/Toasts.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Toast from 'react-bootstrap/Toast';
 import ToastContainer from 'react-bootstrap/ToastContainer';
 
 // https://react-bootstrap.netlify.app/docs/components/toasts/
 const Toasts = () => {
+  
+  const [toasts, setToasts] = useState([]) // [{title: toast title, message: toast messsage, show: true/false}]
+
+  function addToast(title, message){
+    setToasts([...toasts, {title: title, message: message}])
+  }
+
+  console.log(toasts)
+
   return (
-    <ToastContainer>
-        <Toast>
-            <Toast.Header>
-            <strong className="me-auto">Bootstrap</strong>
-            <small className="text-muted">just now</small>
-            </Toast.Header>
-            <Toast.Body>See? Just like this.</Toast.Body>
-        </Toast>
+    <ToastContainer position='top-center'>
+
+        {
+            toasts.map(({title, message}, index) =>{
+                return(
+                    <Toast key={index}>
+                        <Toast.Header>
+                            <strong className="me-auto">{title}</strong>
+                            <small className="text-muted">just now</small>
+                        </Toast.Header>
+                        <Toast.Body>{message}</Toast.Body>
+                    </Toast>
+                )
+            })  
+        }
+    
     </ToastContainer>
   )
 }
