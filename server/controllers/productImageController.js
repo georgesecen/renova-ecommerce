b2b8fc949451a3ffe7b2191f1608a52a5d074793
George Secen <W0533223@myscc.ca>
2025-03-02 16:19:40 -0500

    Add function createProductImage
    - Added service function createProductImage which creates an image for product/product variant in
    the database and on the server itself. The purpose of this function is so that it can be used to create images
    for products (which do not require image on Stripe) and product variants (which do require image on Stripe).
    This way we do not have to use the exact same logic in multiple locations we can just use this function.
    
    - Replaced the logic in the productImageController to use this service function when creating images

--- server/controllers/productImageController.js
+++ server/controllers/productImageController.js
@@ -1,6 +1,7 @@
 const Image = require('../models/productImageModel');
 const fs = require('fs')
-const path = require('path')
+const path = require('path');
+const { createProductImage } = require('../services/productVariantIntegrationService');
 
 /**
  * Creates an image in the database and adds image to the server.
@@ -14,32 +15,7 @@ exports.createImage = async (request, response) => {
 
     try{
 
-        // Add image details to database
-        const image = await Image.create({
-            product_id: productId,
-            product_variant_id: productVariantId,
-            is_primary: isPrimary,
-        })
-
-        // Note: When working with paths avoid using slashes at all costs as different operating 
-        // systems use different slashes
-
-        // Get path to images folder on server
-        const serverPath = path.dirname(__dirname)
-        const imagesPath = path.join(serverPath, "public", "images")   
-        
-        // Image name on server will be primary key of corresponding image in database followed by
-        // the image extension
-        const imageName = `${image.id}${path.extname(originalname)}`
-        
-        // Add image to server
-        // Note: Images with same name will be overwritten.
-        await fs.promises.writeFile(path.join(imagesPath, imageName), buffer)
-
-        // Update image_url to the image added to server
-        await image.update({
-            image_url: imageName
-        })
+        await createProductImage(productId, productVariantId, isPrimary, originalname, buffer)
 
         console.log("Image created successfully in database and added to server.")
         response.status(200).json({
