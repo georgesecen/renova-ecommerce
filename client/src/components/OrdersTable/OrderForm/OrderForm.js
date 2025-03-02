74d4da165a524723b1066fdb46235f40dfdaba5c
George Secen <W0533223@myscc.ca>
2025-03-01 22:35:38 -0500

    Get order form information

--- client/src/components/OrdersTable/OrderForm/OrderForm.js
+++ client/src/components/OrdersTable/OrderForm/OrderForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./orderForm.css"
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
@@ -8,17 +8,30 @@ const OrderForm = () => {
 
   const [show, setShow] = useState(true)
 
+  function getFormData(){
+    const formData = new FormData(document.getElementById("order-form"))
+    console.log(Object.fromEntries(formData.entries()))
+  }
+  
+  // TODO: Style everything
   return (
     <Modal show={show} onHide={() => setShow(false)} centered>
         <Modal.Header closeButton>
             <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+            <form id='order-form'>
+                <input name="status" value="shipped" type="radio"/>Shipped <br></br>
+                <input name="status" value="canceled" type="radio"/>Canceled <br></br>
+                <input name="status" value="pending" type="radio"/>Pending <br></br>
+                <input name="status" value="completed" type="radio"/>Completed <br></br>
+            </form>
+        </Modal.Body>
         <Modal.Footer>
             <Button variant="secondary" onClick={() => setShow(false)}>
                 Close
             </Button>
-            <Button variant="primary" onClick={() => setShow(false)}>
+            <Button variant="primary" onClick={() => {setShow(false); getFormData()}}>
                 Save Changes
             </Button>
         </Modal.Footer>
