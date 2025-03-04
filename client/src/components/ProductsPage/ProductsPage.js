686ae4f5dac57085c8a80bfdaab30cb459646c76
Cameron <w0796224@myscc.ca>
2025-03-04 12:27:31 -0500

    adding/removing cart items properly with new components

--- client/src/components/ProductsPage/ProductsPage.js
+++ client/src/components/ProductsPage/ProductsPage.js
@@ -3,6 +3,9 @@ import './productsPage.css'
 import ProductCard from '../ProductCard/ProductCard';
 import { getProducts } from '../../services/products';
 import { useNavigate } from 'react-router-dom';
+import { addProduct } from "../../services/cart";
+import { useCart } from "../../providers/CartContext";
+import { useUser } from "../../providers/UserContext";
 
 
 function ProductsPage() {
@@ -12,11 +15,21 @@ function ProductsPage() {
     const [filteredProducts, setFilteredProducts] = useState([]);
     const [loading, setLoading] = useState(true);
 
+    const [showModal, setShowModal] = useState(false);
+    const [modalContent, setModalContent] = useState({});
+    const [value, setValue] = useState(1);
+    const { updateCartQuantity } = useCart();
+    const { user } = useUser();
+
+    const user_id = user;
+
+
     useEffect(() => {
         console.log(products)
         getProducts()
             .then((response) => {
                 console.log(response.data[0].image[0]);
+                console.log(response.data[0].image[0].image_url);
                 setProducts(response.data);
                 setFilteredProducts(response.data)  // Populate filtering array
                 setTimeout(() => setLoading(false), 100);  // Show spinner for 200ms
@@ -26,6 +39,34 @@ function ProductsPage() {
                 setLoading(false);
             });
     }, []);
+    //Function to add to cart - pass it through props
+    const addToCartHandler = (product) => {
+        const productData = {
+            user_id: user_id,
+            product_variant_id: product.id,
+            quantity: value,
+        };
+        console.log(productData);
+        addProduct(productData)
+            .then(() => {
+                // Update the cart quantity both in context and localStorage
+                let currentQuantity = parseInt(localStorage.getItem('cartQuantity'), 10) || 0;
+                const newQuantity = currentQuantity + productData.quantity;
+                updateCartQuantity(newQuantity);  // Update context
+                // localStorage.setItem('cartQuantity', newQuantity);  // Persist in localStorage
+                //Set content for modal
+                console.log(product.image[0].image_url);
+                setModalContent({
+                    title: "Product Added to Cart",
+                    message: `${product.name} has been successfully added to your cart.`,
+                    image: `/images/${product.image[0].image_url}`,
+                });
+                setShowModal(true)
+            })
+            .catch((error) => {
+                console.error('Error adding product:', error);
+            });
+    };
 
     // Temporary filtering method
     const filterProducts = (id) => {
@@ -85,7 +126,11 @@ function ProductsPage() {
 
                     // img={`images/${product.image[0].image_url}`} 
                     name={product.name} 
-                    price={product.price}>
+                    price={product.price}
+                    // image={product.image[0].image_url}
+                    image={`/images/${product.image[0].image_url}`}
+                                 // product_image: item.productVariant.product.image[0].image_url
+                    addToCart={() => addToCartHandler(product)}>
                         
                     </ProductCard>
                 ))}
