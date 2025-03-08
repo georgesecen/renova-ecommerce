2271a36dced3043edff29eb76b3023de46459fb4
George Secen <W0533223@myscc.ca>
2025-03-08 14:34:33 -0500

    Add trash icon for the image delete button

--- client/src/components/ProductsTable/ImageForm/ImageForm.js
+++ client/src/components/ProductsTable/ImageForm/ImageForm.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react'
 import "./imageForm.css"
 import { adminProductsService } from '../../../services/products';
 import Modal from 'react-bootstrap/Modal';
+const trashIcon = require("../../../assets/icons/trash.png")
 
 
 const ImageForm = ({productImages}) => {
 
   // Keep track of image name for image which is currently selected to be deleted
-  const [imageToDelete, setImageToDelete] = useState([])
+  const [imageToDelete, setImageToDelete] = useState()
 
   function addImage(file){
 
@@ -29,9 +30,9 @@ const ImageForm = ({productImages}) => {
           {
             productImages.map((image, index) => {
               return (
-                <div key={index} onClick={() => setImageToDelete(image)} className={`image-container ${image === imageToDelete ? "show" : ""}`}>
+                <div key={index} onClick={() => setImageToDelete(imageToDelete === image ? null : image)} className={`image-container ${image === imageToDelete ? "show" : ""}`}>
                   <img src={`http://localhost:3306/static/images/${image}`} alt='product'/>
-                  <button>delte</button>
+                  <button><img alt='delete' src={trashIcon}/></button>
                 </div>
               )
             })
