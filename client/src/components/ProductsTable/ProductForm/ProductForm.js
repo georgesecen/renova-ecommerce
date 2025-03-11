bdc16f938e960990be60f16b5e7af9df121a17dd
George Secen <W0533223@myscc.ca>
2025-03-11 17:14:06 -0400

    Process product form data

--- client/src/components/ProductsTable/ProductForm/ProductForm.js
+++ client/src/components/ProductsTable/ProductForm/ProductForm.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./productForm.css"
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
-const ProductForm = ({productId, update, show, setShow}) => {
+const ProductForm = ({productId, update, show, setShow, description, price}) => {
+
+  // Gets data from form and updates product
+  function processFormData(){
+    const formData = new FormData(document.getElementById("product-form"))
+    const entries = Object.fromEntries(formData.entries()) // Get key value pairs (Keys being form feild names)
+
+    // Only if description or price was changed update product
+    if (entries.description !== description || Number(entries.price) !== price){
+        update(productId, entries.description, Number(entries.price))
+    }
+  }
+  
   return (
     <Modal show={show} onHide={() => setShow(false)} centered>
         <Modal.Header closeButton>
             <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            form
+            <form id='product-form'>
+                <textarea name='description' defaultValue={"textAreaDescription"}></textarea>
+                <input name='price' defaultValue={10.92} step={1} type='number' />
+            </form>
         </Modal.Body>
         <Modal.Footer>
             <Button variant="secondary" onClick={() => setShow(false)}>
                 Close
             </Button>
-            <Button variant="primary" onClick={() => {setShow(false)}}>
+            <Button variant="primary" onClick={() => {setShow(false); processFormData()}}>
                 Save Changes
             </Button>
         </Modal.Footer>
