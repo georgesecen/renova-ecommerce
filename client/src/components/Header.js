69b5957fcf817c7d25b52c94190967c08f8d7a7d
Cameron <w0796224@myscc.ca>
2025-03-16 13:14:22 -0400

    fixed header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,58 +4,35 @@ import { CgShoppingCart } from 'react-icons/cg'
 import '../assets/images/logo.png'
 import { useUser } from "../providers/UserContext";
 import { useCart } from "../providers/CartContext";
-<<<<<<< HEAD
-import { logoutUser } from "../services/user";
-import { useNavigate } from "react-router-dom";
-import {getCartItemQuantity} from "../services/cart";
-=======
->>>>>>> development
 
 function Header() {
-  const { totalQuantity } = useCart();
-  const { isLoggedIn } = useUser();
-  const logo = require('../assets/images/logo.png');
+    const { totalQuantity } = useCart();
+    const { isLoggedIn } = useUser();
+    const logo = require('../assets/images/logo.png');
 
-<<<<<<< HEAD
-    const navigate = useNavigate();
-    //Handle logout
-    const logoutHandler = async () => {
-        try {
-            await logoutUser();
-            logout();
-            const res = await getCartItemQuantity()
-            const guestUserCartQuantity = localStorage.getItem("cartQuantity")
-            updateCartQuantity(guestUserCartQuantity);
-            // localStorage.removeItem('cartQuantity');
-            navigate('/');
-        } catch (error) {
-            console.error("logoutUser");
-            throw error;
-        }
-    }
-  return (
-    <div className="header">
-      <div className="headerContent">
-        <div className="headerLogo">
-          <img src={logo} alt="" />
-          RENOVA
-        </div>
-            <div className="headerNav">
-                <NavLink to='/'>HOME</NavLink>
-                <NavLink to='/products'>PRODUCTS</NavLink>
-                <NavLink to='/contact'>CONTACT</NavLink>
-            </div>
-            <div className="headerNav2">
-                {isLoggedIn ? (
-                    <NavLink to="/dashboard">PROFILE</NavLink>
-                ) : (
-                    <NavLink to="/signIn">SIGN IN</NavLink>
-                )}
-                <NavLink to='/cart'> {totalQuantity} {<CgShoppingCart />}</NavLink>
+    return (
+        <div className="header">
+            <div className="headerContent">
+                <div className="headerLogo">
+                    <img src={logo} alt="" />
+                    RENOVA
+                </div>
+                <div className="headerNav">
+                    <NavLink to='/'>HOME</NavLink>
+                    <NavLink to='/products'>PRODUCTS</NavLink>
+                    <NavLink to='/contact'>CONTACT</NavLink>
+                </div>
+                <div className="headerNav2">
+                    {isLoggedIn ? (
+                        <NavLink to="/dashboard">PROFILE</NavLink>
+                    ) : (
+                        <NavLink to="/signIn">SIGN IN</NavLink>
+                    )}
+                    <NavLink to='/cart'> {totalQuantity} {<CgShoppingCart />}</NavLink>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Header
