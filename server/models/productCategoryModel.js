d6241c1373ff557263249fd405a4f4cafac05865
George Secen <W0533223@myscc.ca>
2025-03-16 19:10:59 -0400

    Create ProductCategory model to represent product categories

---
+++ server/models/productCategoryModel.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database')
+
+const ProductCategory = sequelize.define(
+    'productCategory', 
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        tableName: 'product_categories',
+        underscored: true,
+        timestamps: true,
+    }
+)
+
+module.exports = ProductCategory;
