9d0579b72bfc0c6c4ddd3d25d541e19dcfc98f33
George Secen <W0533223@myscc.ca>
2025-03-16 21:12:02 -0400

    Get all product categories from CategoriesTable component

--- client/src/components/CategoriesTable/CategoriesTable.js
+++ client/src/components/CategoriesTable/CategoriesTable.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { adminProductCategoriesService } from '../../services/productCategories'
 
 const CategoriesTable = ({ displayNotification }) => {
+
+  const [categories, setCategories] = useState([])
+  const [loadCategories, setLoadCategories] = useState(true) // When set to true will trigger reload of products
+
+  // Get product categories
+  useEffect(() => {
+      if (loadCategories){
+        adminProductCategoriesService("index")
+          .then((response) => setCategories(response.data.data))
+          .catch((error) => displayNotification("Get", `${error}`, "danger"))
+          .finally(() => setLoadCategories(false))   
+      }  
+  }, [loadCategories])
+
+  console.log(categories)
+
+
   return (
     <div>CategoriesTable</div>
   )
