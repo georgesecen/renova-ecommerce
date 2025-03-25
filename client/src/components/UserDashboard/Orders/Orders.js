9a05a851d0f9ac82a6226b0a4d256c83b9ad1256
George Secen <W0533223@myscc.ca>
2025-03-24 23:53:30 -0400

    Display customers order details in user dashboard

--- client/src/components/UserDashboard/Orders/Orders.js
+++ client/src/components/UserDashboard/Orders/Orders.js
@@ -1,22 +1,63 @@
-import React, { useState, useEffect } from 'react'
-import { getUserOrders } from '../../../services/orders'
+import React from 'react'
 import "./orders.css"
 
-const Orders = ({userId}) => {
+const Orders = ({orders}) => {
 
-  const [orders, setOrders] = useState([])
+
+  // Displays details about each customer order
+  const OrderItems = ({order}) => {
+
+    // Get all order details
+    const {
+      id,
+      total_price: total,
+      status,
+      updated_at: lastUpdated,
+      order_items: orderItems
+    } = order
+
+    return (
+      <div>
+        <div className='order-items-header'>
+          <h3>Order #: {id}</h3>
+          <p>Order Details</p>
+        </div>
+        <p>{lastUpdated} | {status} | Total: ${total}</p>
+
+        {/* Actual products which were apart of order */}
+        <ul>
+          {
+            orderItems.map((orderItem, index) => {
+
+              // Get order item details
+              const { quantity } = orderItem
+              const { color, size, product } = orderItem.product_variant
+              const { name, gender } = product
+
+              return (
+                <li key={index}>
+                  <h2>{name}</h2>
+                  <h4>{color}-{size}-{gender}</h4>
+                  <h4>Qty: {quantity}</h4>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    )
+  } 
+
   console.log(orders)
-  
-  // Get user orders
-  useEffect(() => {
-      getUserOrders(userId)
-        .then((response) => setOrders(response.data.data))
-        .catch((error) => console.log(error))
-    
-  }, [])
 
   return (
-    <div>Orders</div>
+    <div>
+      {
+        orders.map((order, index) => {
+          return <OrderItems order={order} key={index} />
+        })
+      }
+    </div>
   )
 }
 
--- client/src/components/UserDashboard/UserDashboard.js
+++ client/src/components/UserDashboard/UserDashboard.js
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Home from "./Home/Home";
 import Orders from "./Orders/Orders";
-import {logoutUser} from "../../services/user";
+import { logoutUser } from "../../services/user";
 import { useUser } from "../../providers/UserContext";
 import { useCart } from "../../providers/CartContext";
 import { useNavigate } from "react-router-dom";
+import { getUserOrders } from "../../services/orders";
 import "./UserDashboard.css";
 
 const UserDashboard = () => {
     const [activeSection, setActiveSection] = useState("home");
     const { totalQuantity, updateCartQuantity } = useCart();
     const { isLoggedIn, logout, user } = useUser();
+    const [orders, setOrders] = useState([])      
     const navigate = useNavigate();
+
+    // Get user orders
+    useEffect(() => {
+        if (user !== null){
+            getUserOrders(user.userId)
+            .then((response) => setOrders(response.data.data))
+            .catch((error) => console.log(error))
+        }
+    }, [user])
 
     //Handle logout
     const logoutHandler = async () => {
@@ -38,7 +49,7 @@ const UserDashboard = () => {
             </div>
 
             {activeSection === "home" && <Home logout={logoutHandler} />}
-            {activeSection === "orders" && <Orders userId={user.userId}/>}
+            {activeSection === "orders" && <Orders orders={orders}/>}
         </div>
     );
 };
